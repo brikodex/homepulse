6282fe0ddf17660192e9d192913b64acf6666673
bricio-imbiki <bricio.imbiki15@gmail.com>
2025-12-09 17:44:17 +0300

    feat: Configure server-side rendering routes for product and admin pages with default prerendering.

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/products/edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
